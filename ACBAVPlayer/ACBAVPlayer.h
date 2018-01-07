1ea25c695732e574f1d53ca18547ee46f853a0f9
Akhil <akhilcb@yahoo.com>
2018-01-07 15:00:51 -0800

    Added framework target

---
+++ ACBAVPlayer/ACBAVPlayer.h
@@ -0,0 +1,23 @@
+//
+//  ACBAVPlayer.h
+//  ACBAVPlayer
+//
+//  Created by Akhil C Balan on 1/7/18.
+//  Copyright © 2018 akhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACBAVPlayer.
+FOUNDATION_EXPORT double ACBAVPlayerVersionNumber;
+
+//! Project version string for ACBAVPlayer.
+FOUNDATION_EXPORT const unsigned char ACBAVPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACBAVPlayer/PublicHeader.h>
+
+
+#import <ACBAVPlayer/AVPlayer+ACBHelper.h>
+#import <ACBAVPlayer/ACBAudioProcessHelper.h>
+#import <ACBAVPlayer/MYAudioTapProcessor.h>
+
